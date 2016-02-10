858e2c3bb4ec31db4c15ca995b6abf38d21abdaa
Michael Cheng <mcheng@pdx.edu>
2016-02-09 20:39:57 -0800

    sleep

--- manager.cpp
+++ manager.cpp
@@ -7,12 +7,29 @@ using namespace std;
 void manager::load_menu()
 {
     menu_object = new menu_list;
-    char a [] = "gay";
-    char b [] = "aa";
-    char c [] = "sds"; 
-    menu_object->insert(2, c);
-    menu_object->insert(1, b);
-    menu_object->insert(0,a);
-    menu_object->display_all();
+    int size = 10;
+
+    char type[size], menu_item[size];
+
+    ifstream load;
+    load.open("me.txt");
+    if(load)
+    {
+        while(!load.eof())
+        {
+            load.getline(type, size, '\n');
+            load.getline(menu_item, size, '\n');
+            load.ignore(size, '\n');
+
+            int t = (int)*type;
+            menu_object->insert(t, menu_item);
+            menu_object->display_all();
+
+        }
+        
+    }
+    load.close();
+    return;
+
 }
 
